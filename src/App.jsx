79da07a382699ca19844913fd195e7fa47cf9079
Julien <newdevcreator@gmail.com>
2024-02-24 14:32:10 +0100

    FEAT: redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { useSelector, useDispatch } from "react-redux";
+import { getNotesFromAPI } from "./features/notes";
+import NotesList from "./components/NotesList";
+
 function App() {
-  return <></>
+  const dispatch = useDispatch();
+  const notes = useSelector((state) => state.notes);
+
+  if (!notes.list) {
+    dispatch(getNotesFromAPI());
+  }
+
+  console.log(notes);
+  return (
+    <>
+      <NotesList />
+    </>
+  );
 }
 
-export default App
+export default App;
