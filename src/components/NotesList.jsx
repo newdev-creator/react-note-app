d3dc8d9fa0c7cca45c13ea7c8490778b6f1d2e7f
Julien <newdevcreator@gmail.com>
2024-02-28 11:21:41 +0100

    FIX: link

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 export default function NotesList() {
   const notes = useSelector((state) => state.notes);
@@ -7,18 +8,20 @@ export default function NotesList() {
   return (
     <div className="p-10 w-full">
       <p className="text-xl text-slate-200 mb-6">Bievenue sur Notes101</p>
-      <div className="grid lg:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-5 gap-6">
+      <ul className="grid lg:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-5 gap-6">
         {notes.list?.length > 0 &&
           notes.list.map((note) => (
-            <div
+            <li
               key={note.id}
-              className="bg-slate-200 hover:bg-slate-50 p-4 rounded cursor-pointer"
+              className="bg-slate-200 hover:bg-slate-50 rounded cursor-pointer"
             >
-              <p className="text-lg font-semibold">{note.title}</p>
-              <p className="text-gray-700">{note.subtitle}</p>
-            </div>
+              <Link to={`/note/${note.id}`} className="block p-4 w-full h-full">
+                <p className="text-lg font-semibold">{note.title}</p>
+                <p className="text-gray-700">{note.subtitle}</p>
+              </Link>
+            </li>
           ))}
-      </div>
+      </ul>
     </div>
   );
 }
